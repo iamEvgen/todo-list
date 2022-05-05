9bb3eb735067d258ce599951bcce06e9f19dd7b3
iamEvgen <evgen.alexa@gmail.com>
2022-05-05 18:14:44 +0300

    add project list with delete button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import './img/menu.png'
 import './img/home.png'
 import './img/GitHub.png'
+import closeBtn from './icons/del.png'
 
 
 const domManipulation = (function() {
@@ -9,6 +10,10 @@ const domManipulation = (function() {
   const content = document.querySelector('.content');
   const menuBtn = document.querySelector('.menu');
   const homeBtn = document.querySelector('.home');
+  const addProjectBtn = document.querySelector('.addProjectBtn');
+  const addProjectForm = document.querySelector('#addProjectForm');
+  const addProjectInput = document.querySelector('#addProjectInput');
+  const projectsList = document.querySelector('.projectsList');
   let sidebarShow = true;
 
   function hideShowSidebar() {
@@ -23,6 +28,36 @@ const domManipulation = (function() {
     }
   }
 
+  function showAddProjectForm() {
+    addProjectForm.classList.remove('hidden');
+    addProjectInput.focus();
+    addProjectInput.value = '';
+  }
 
+  function addProject(event) {
+    event.preventDefault();
+    const action = event.submitter.classList[0];
+    if (action === 'submitNewProject') {
+      addProjectForm.classList.add('hidden');
+      const newProject = document.createElement('div');
+      const projectName = document.createElement('div');
+      newProject.classList.add('projectItem');
+      const text = addProjectInput.value.charAt(0).toUpperCase() + addProjectInput.value.slice(1).toLowerCase();
+      projectName.textContent = text;
+      newProject.appendChild(projectName);
+
+      const delIcon = new Image();
+      delIcon.src = closeBtn;
+      delIcon.classList.add('delBtn');
+      newProject.appendChild(delIcon);
+
+      projectsList.appendChild(newProject);
+    } else if (action === 'cancelNewProject') {
+      addProjectForm.classList.add('hidden');
+    }
+  }
+
+  addProjectForm.addEventListener('submit', addProject);
   menuBtn.addEventListener('click', hideShowSidebar);
+  addProjectBtn.addEventListener('click', showAddProjectForm);
 })();
